ce582fa0dd45960f91fcabc5a2cae61b2393b250
Minzhi Qu <quminzhi@gmail.com>
2022-02-23 10:36:13 -0800

    Added Count Univalue Subtrees

--- DataStructure/BinaryTree/solution.cpp
+++ DataStructure/BinaryTree/solution.cpp
@@ -2,6 +2,7 @@
 
 #include <queue>
 #include <stack>
+#include <unordered_map>
 using namespace std;
 
 /**
@@ -316,11 +317,64 @@ bool BinaryTree::isUnivalTree(TreeNode* root) {
 
 /**
  * @brief A uni-value subtree means all nodes of the subtree have the same value.
- * The solution is based on that of isUnivalTree
+ * The solution is based on that of isUnivalTree.
+ *
+ * There are two cases where coalescence will happen (increase 1):
+ * 1. only one child and root->val == child->val and child is UnivalTree
+ * 2. two children and root has the same val and count(left) == count(right) == 1 and
+ * both of them are UnivalTree
+ *
+ * count(root) = count(left) + count(right) otherwise.
  * @param root
  * @return int
  */
-int BinaryTree::countUnivalSubtrees(TreeNode* root) {}
+int BinaryTree::countUnivalSubtreesHelper(TreeNode* root,
+                                          unordered_map<TreeNode*, bool>& isUnival) {
+    if (root == nullptr) return 0;   // corner case
+
+    // both children are nullptr
+    if ((root->left == nullptr) && (root->right == nullptr)) {
+        isUnival[root] = true;
+        return 1;
+    }
+    // only left: let it go down first, check if it is UnivalSubtree when backtracking
+    if (root->right == nullptr) {
+        int count = countUnivalSubtreesHelper(root->left, isUnival);   // go down first
+        if ((isUnival[root->left]) && (root->val == root->left->val)) {
+            isUnival[root] = true;
+            return ++count;
+        }
+        return count;
+    }
+    // only right
+    if ((root->left == nullptr)) {
+        int count = countUnivalSubtreesHelper(root->right, isUnival);
+        if ((isUnival[root->right]) && (root->val == root->right->val)) {
+            isUnival[root] = true;
+            return ++count;
+        }
+        return count;
+    }
+    // both are non-nullptr: 2 cases
+    int leftCount = countUnivalSubtreesHelper(root->left, isUnival);
+    int rightCount = countUnivalSubtreesHelper(root->right, isUnival);
+    int count = leftCount + rightCount;
+    // case 1: coalesce
+    if ((root->left != nullptr) && isUnival[root->left] &&
+        (root->val == root->left->val) && (root->right != nullptr) &&
+        isUnival[root->right] && (root->val == root->right->val)) {
+        isUnival[root] = true;
+        return ++count;
+    }
+    // case 2: no coalescence
+    isUnival[root] = false;
+    return count;
+}
+
+int BinaryTree::countUnivalSubtrees(TreeNode* root) {
+    unordered_map<TreeNode*, bool> isUnival;
+    for () return countUnivalSubtreesHelper(root, isUnival);
+}
 
 // write your solution here
 void Solution() {}
