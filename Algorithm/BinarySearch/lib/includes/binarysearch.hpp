4663dba4f87990c8a0ff317603c6adf40384fa4f
Minzhi Qu <quminzhi@gmail.com>
2022-05-14 23:02:01 -0700

    Added merge intervals

--- Algorithm/BinarySearch/lib/includes/binarysearch.hpp
+++ Algorithm/BinarySearch/lib/includes/binarysearch.hpp
@@ -31,7 +31,7 @@ bool isBadVersion(int version);
 int findPeakElementI(vector<int>& nums);
 int findPeakElementII(vector<int>& nums);
 int findMin(vector<int>& nums);
-int findMinII(vector<int>& nums); // with duplicates
+int findMinII(vector<int>& nums);   // with duplicates
 
 vector<int> searchRange(vector<int>& nums, int target);
 vector<int> findClosestElementsI(vector<int>& arr, int k, int x);
@@ -44,7 +44,7 @@ class ArrayReader {
 };
 int searchI(const ArrayReader& reader, int target);
 int searchII(const ArrayReader& reader, int target);
-double myPow(double x, long n); // allow negative power
+double myPow(double x, long n);   // allow negative power
 bool isPerfectSquare(int num);
 char nextGreatestLetter(vector<char>& letters, char target);
 
@@ -61,5 +61,109 @@ int splitArrayHelper(vector<int>& nums, vector<int>& sum, int begin, int m);
 int mySum(vector<int>::iterator begin, vector<int>::iterator end);
 int myMax(vector<int>::iterator begin, vector<int>::iterator end);
 int splitArrayII(vector<int>& nums, int m);
+
+class CountIntervals {
+   public:
+    vector<int> lv;
+    vector<int> rv;
+    int sz;
+
+    CountIntervals() : sz(0){};
+
+    void add(int left, int right) {
+        if (lv.empty()) {
+            lv.push_back(left);
+            rv.push_back(right);
+            sz += right - left + 1;
+            return;
+        }
+        // find the idx1 in rv the first greater or equal than left
+        int idx1 = greaterThan(left);
+        // find the idx2 in lv the first less or equal than right
+        int idx2 = lessThan(right);
+
+        // no overlap
+        if (left > rv[idx1]) {
+            // largest interval
+            lv.push_back(left);
+            rv.push_back(right);
+            sz += right - left + 1;
+            return;
+        }
+        if (right < lv[idx2]) {
+            // smallest interval
+            lv.insert(lv.begin(), left);
+            rv.insert(rv.begin(), right);
+            sz += right - left + 1;
+            return;
+        }
+
+        if (idx1 > idx2) {
+            // insert between idx1 and idx2
+            lv.insert(lv.begin() + idx1, left);
+            rv.insert(rv.begin() + idx1, right);
+            sz += right - left + 1;
+        }
+
+        // overlap with only one interval
+        if (idx1 == idx2) {
+            int newLeft = min(left, lv[idx1]);
+            int newRight = max(right, rv[idx1]);
+            sz += (newRight - newLeft + 1) - (rv[idx1] - lv[idx1] + 1);
+            lv[idx1] = newLeft;
+            rv[idx1] = newRight;
+        }
+        // overlap with multiple intervals
+        if (idx1 < idx2) {
+            // merge intervals and update count
+            int newLeft = min(left, lv[idx1]);
+            int newRight = min(right, rv[idx2]);
+            // delete intervals from idx1 to idx2 and update 'sz'
+            for (int i = idx1; i <= idx2; i++) {
+                sz = sz - (rv[i] - lv[i] + 1);
+            }
+            lv.erase(lv.begin() + idx1, lv.begin() + idx2 + 1);
+            rv.erase(rv.begin() + idx1, rv.begin() + idx2 + 1);
+
+            // insert new intervals
+            sz += newRight - newLeft + 1;
+            lv.insert(lv.begin() + idx1, newLeft);
+            rv.insert(rv.begin() + idx1, newRight);
+        }
+    }
+
+    int count() { return sz; }
+
+   private:
+    // find the first greater or equal to left in vector 'rv'
+    int greaterThan(int left) {
+        int l = 0;
+        int r = rv.size() - 1;
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (rv[mid] < left) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
+    }
+
+    // find the first less or equal to right in vector 'lv'
+    int lessThan(int right) {
+        int l = 0;
+        int r = lv.size() - 1;
+        while (l < r) {
+            int mid = l + ((r - l + 1) >> 1);
+            if (lv[mid] > right) {
+                r = mid - 1;
+            } else {
+                l = mid;   // add 1 to mid to prevent dead loop
+            }
+        }
+        return l;
+    }
+};
 
 #endif
